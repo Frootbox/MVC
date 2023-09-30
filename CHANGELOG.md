b71c6e3c289bb8e44d1d2a9b41c95f90b67f81ef
Jan Habbo Brüning <jan.habbo.bruening@gmail.com>
2023-09-30 08:08:02 +0200

    - Added cache support.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,7 +6,11 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
-## [0.4.1] - 2023-09-30
+## [0.5] - 2023-09-30
+
+## Added
+
+- Added cache support.
 
 ## Fixed
 
--- src/Translator.php
+++ src/Translator.php
@@ -12,6 +12,21 @@ class Translator {
 
     /**
      * @param string $file
+     * @return void
+     * @throws \Exception
+     */
+    public function addCacheFile(string $file): void
+    {
+        if (!file_exists($file)) {
+            throw new \Exception('Cache file does not exist.');
+        }
+
+        // Set data from cache
+        $this->setData(require $file);
+    }
+
+    /**
+     * @param string $file
      * @param string $language
      * @return void
      */
@@ -21,11 +36,44 @@ class Translator {
     }
 
     /**
+     * @return array
+     */
+    public function getData(): array
+    {
+        return $this->data ?? [];
+    }
+
+    /**
+     * @param array $data
+     * @return void
+     */
+    public function setData(array $data): void
+    {
+        $this->data = $data;
+    }
+
+    /**
      * @param string $key
      * @return string|null
      */
     public function translate(string $key): ?string
     {
         return $this->data[$this->languagePrimary][$key] ?? $key;
+    }
+
+    /**
+     * @param string $cacheFile
+     * @return void
+     * @throws \Exception
+     */
+    public function writeCache(string $cacheFile): void
+    {
+        // Generate cache file source
+        $source = '<?php' . PHP_EOL . 'return ' . var_export($this->getData(), true) . ';' . PHP_EOL;
+
+        // Write cache file
+        $file = new \Frootbox\Filesystem\File($cacheFile);
+        $file->setSource($source);
+        $file->write();
     }
 }
